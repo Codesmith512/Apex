4b1efa9895731199616fac6b4910230bf6277963
John Wargo <7339876+Codesmith512@users.noreply.github.com>
2017-12-23 07:25:40 -0600

    Implemented flat GDT

--- src/boot.asm
+++ src/boot.asm
@@ -45,12 +45,55 @@ stack_top:
 ; Actual executable code goes here!
 ; ##################################
 section .text
+
+; Define a flat GDT that spans the whole 4GiB space
+GDT:
+  dw .table_end - .table_start - 1
+  dd .table_start
+.table_start:
+  ; 0x00 Null selector -- unusable
+  dq 0
+  ; 0x08 Code selector
+  dw 0xffff     ;  0:15b Limit
+  dw 0x0000     ;  0:15b Base
+  db 0x00       ; 16:23b Base
+  db 10011010b  ;  0: 7b Access Flags
+  db 0xcf       ; 16:19b Limit
+                ;  0: 4b Descriptor Flags
+  db 0x00       ; 24:31b Base
+  ; 0x10 Data selector
+  dw 0xffff     ;  0:15b Limit
+  dw 0x0000     ;  0:15b Base
+  db 0x00       ; 16:23b Base
+  db 10010010b  ;  0: 7b Access Flags
+  db 0xcf       ; 16:19b Limit
+                ;  0: 4b Descriptor Flags
+  db 0x00       ; 24:31b Base
+.table_end:
+
+; @func [[noreturn]] void _boot(void)
+; The initial entry point -- returning is UB
 global _boot
 _boot:
   mov esp, stack_top    ; Setup the stack
+  call loadGDT          ; Setup the flat GDT and registers
   call kernel_main      ; Invoke kernel
   cli                   ; Clear interrupts to prevent crashes
 .hang:
   hlt                   ; Stop the processor!
   jmp .hang             ; Stop the processor again...
 .end:
+
+; @func void loadGDT(void)
+; Loads the GDT and sets the new segments accordingly
+loadGDT:
+  lgdt [GDT]
+  jmp 0x0008:.reload_segments
+  .reload_segments
+  mov ax, 0x10
+  mov ds, ax
+  mov es, ax
+  mov fs, ax
+  mov gs, ax
+  mov ss, ax
+  ret
